bcf801219bf18d037b325346c924ffa85c49a749
MarioToast <70570052+MarioToast@users.noreply.github.com>
2020-09-01 13:27:21 +0200

    sd

--- HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
+#include <string>
 
 int age;
+std::string Fullname = "";
 
 int main() {
-	std::cout << "Enter your age :";
-	std::cin >> age;
-	std::cout << "Your age is " << age << "\n";
-	std::cout << "You are young.";
+	std::cout << "Enter your full name";
+	std::getline(std::cin, Fullname);
+	std::cout << "Your full name is " << Fullname;
+	//std::cout << "Your age is " << age << "\n";
+	//std::cout << "You are young.";
 }
